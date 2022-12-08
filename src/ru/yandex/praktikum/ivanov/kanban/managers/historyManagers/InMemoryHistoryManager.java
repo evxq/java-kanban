ad09678c94c796baca585e1992bdeb2cf9c4986b
eugene <olympian2008@yandex.ru>
2022-12-08 13:38:44 +0300

    fix: ТЗ-5 исправление замечаний-2

--- src/ru/yandex/praktikum/ivanov/kanban/managers/historyManagers/InMemoryHistoryManager.java
+++ src/ru/yandex/praktikum/ivanov/kanban/managers/historyManagers/InMemoryHistoryManager.java
@@ -11,18 +11,13 @@ public class InMemoryHistoryManager implements HistoryManager {
     private CustomLinkedList<Task> customList = new CustomLinkedList<>();       // двусвязный список для хранения истории_просмотров
 
     @Override
-    public void add(Task task) {
-        // если задача содержится в HashMap, то удалить ее из списка (removeNode), а затем добавить в конец списка (и обновить значение узла)
-        // иначе: добавить задачу в конец списка и в HashMap
-        if (customList.historyMap.containsKey(task.getId())) {
-            customList.removeNode((task.getId()));
-        }
+    public void add(Task task) {                            // <ТЗ-5> удалить задачу из списка (removeNode), а затем добавить в конец списка
+        customList.removeNode((task.getId()));
         customList.linkLast(task);
-        customList.historyMap.put(task.getId(), customList.tail);
     }
 
     @Override
-    public void remove(int id) {                               // <ТЗ-5> удаление задач из истории_просмотра
+    public void remove(int id) {                                           // <ТЗ-5> удаление задач из истории_просмотра
         customList.removeNode(id);
     }
 
@@ -33,54 +28,60 @@ public class InMemoryHistoryManager implements HistoryManager {
 
 
     public class CustomLinkedList<T> {
-        private HashMap<Integer, Node<Task>> historyMap = new HashMap<>();          // <ТЗ-5> В ключах хранится id задач, а в значениях — узлы связного списка
-        private Node<Task> head;
-        private Node<Task> tail;
-        private int size = 0;
+        private HashMap<Integer, Node<T>> historyMap = new HashMap<>();          // <ТЗ-5> В ключах хранится id задач, а в значениях — узлы связного списка
+        private Node<T> head;
+        private Node<T> tail;
 
-        public void linkLast(Task task) {                         // <ТЗ-5> Добавить задачу в конец двусвязного списка
-            final Node<Task> oldTail = tail;
-            final Node<Task> newTail = new Node<Task>(oldTail, task, null);
+        public void linkLast(T task) {                              // <ТЗ-5> Добавить задачу в конец двусвязного списка
+            final Node<T> oldTail = tail;
+            final Node<T> newTail = new Node<T>(oldTail, task, null);
             tail = newTail;
             if (oldTail == null) {
                 head = newTail;
             } else {
-                oldTail.next = newTail;
+                oldTail.setNext(newTail);
             }
-            size++;
+
+            if (task instanceof Task) {
+                Integer id = ((Task) task).getId();
+                historyMap.put(id, tail);
+            } else {
+                System.out.println("Некорректный тип задачи");
+            }
         }
 
-        public ArrayList<Task> getTasks() {                       // <ТЗ-5> собрать все задачи из двусвязного списка в обычный ArrayList
-            ArrayList<Task> taskArray = new ArrayList<>();
-            Node<Task> x = head;
-            while (x != null) {
-                taskArray.add(x.data);
-                x = x.next;
+        public ArrayList<T> getTasks() {                       // <ТЗ-5> собрать все задачи из двусвязного списка в обычный ArrayList
+            ArrayList<T> taskArray = new ArrayList<>();
+            Node<T> element = head;
+            while (element != null) {
+                taskArray.add(element.getData());
+                element = element.getNext();
             }
             return taskArray;
         }
 
-        public void removeNode(int id) {                 // <ТЗ-5> удаление узла из двусвязного списка
-            Node<Task> node = historyMap.get(id);
-            final Node<Task> next = node.next;
-            final Node<Task> prev = node.prev;
+        public void removeNode(int id) {                                   // <ТЗ-5> удаление узла из двусвязного списка
+            if (historyMap.get(id) == null) {
+                return;
+            }
+
+            Node<T> node = historyMap.get(id);
+            final Node<T> next = node.getNext();
+            final Node<T> prev = node.getPrev();
 
             if (prev == null) {
                 head = next;
             } else {
-                prev.next = next;
-                node.prev = null;
+                prev.setNext(next);
+                node.setPrev(null);
             }
 
             if (next == null) {
                 tail = prev;
             } else {
-                next.prev = prev;
-                node.next = null;
+                next.setPrev(prev);
+                node.setNext(null);
             }
-
-            node.data = null;
-            size--;
         }
     }
 }
--- src/ru/yandex/praktikum/ivanov/kanban/managers/historyManagers/Node.java
+++ src/ru/yandex/praktikum/ivanov/kanban/managers/historyManagers/Node.java
@@ -1,13 +1,37 @@
 package ru.yandex.praktikum.ivanov.kanban.managers.historyManagers;
 
 public class Node<E> {
-    public E data;
-    public Node<E> next;
-    public Node<E> prev;
+    private E data;
+    private Node<E> next;
+    private Node<E> prev;
 
     public Node(Node<E> prev, E data, Node<E> next) {
         this.data = data;
         this.next = next;
+        this.prev = prev;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<E> prev) {
         this.prev = prev;
     }
 }
