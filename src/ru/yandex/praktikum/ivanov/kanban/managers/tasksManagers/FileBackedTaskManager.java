0ca774e4631e10af0216b408b8aed7f21fc1e786
eugene <olympian2008@yandex.ru>
2022-12-20 22:22:38 +0300

    ТЗ-6 fix #2

--- src/ru/yandex/praktikum/ivanov/kanban/managers/tasksManagers/FileBackedTaskManager.java
+++ src/ru/yandex/praktikum/ivanov/kanban/managers/tasksManagers/FileBackedTaskManager.java
@@ -1,5 +1,6 @@
 package ru.yandex.praktikum.ivanov.kanban.managers.tasksManagers;
 
+import ru.yandex.praktikum.ivanov.kanban.managers.Managers;
 import ru.yandex.praktikum.ivanov.kanban.managers.historyManagers.HistoryManager;
 import ru.yandex.praktikum.ivanov.kanban.tasks.*;
 
@@ -12,6 +13,77 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
     private FileBackedTaskManager(File file) {
         super();
         this.fileToSave = file;
+    }
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\ev\\dev\\java-kanban\\src\\ru\\yandex\\praktikum\\ivanov\\kanban\\save.csv");
+        FileBackedTaskManager fileTaskManager = FileBackedTaskManager.getDefaultFile(file);
+
+        Task task1 = new Task("Купить кроссовки",
+                "Выделить время в течение недели, чтобы сходить в магазин обуви",
+                Status.NEW);
+        Task task2 = new Task("Повесить картину",
+                "Подобрать место на стене, забить гвоздь и повесить картину",
+                Status.DONE);
+
+        Epic epic1 = new Epic("Приготовить обед", "Приготовить блюда и накрыть на стол", new ArrayList<>());
+        Subtask subtask11 = new Subtask("Сварить суп",
+                "Подготовить и нарезать все ингридиенты. Сварить в кастрюле",
+                Status.DONE, epic1);
+        Subtask subtask12 = new Subtask("Приготовить второе блюдо",
+                "Сварить рис и пожарить котлету",
+                Status.NEW, epic1);
+        Subtask subtask13 = new Subtask("Накрыть на стол",
+                "Разложить блюда в тарелки",
+                Status.NEW, epic1);
+
+
+        Epic epic2 = new Epic("Выучить Java", "Выучить язык Java до уровня Junior", new ArrayList<>());
+        Subtask subtask21 = new Subtask("Записаться на курсы",
+                "Записаться на курсы Yandex Practicum",
+                Status.IN_PROGRESS, epic2);
+
+        // создаем задачи в менеджере работы с файлом
+        fileTaskManager.createTask(task1);          // 1
+        fileTaskManager.createTask(task2);          // 2
+
+        fileTaskManager.createEpic(epic1);          // 3
+        fileTaskManager.createSubtask(subtask11);   // 4
+        fileTaskManager.createSubtask(subtask12);   // 5
+        fileTaskManager.createSubtask(subtask13);   // 6
+
+        fileTaskManager.createEpic(epic2);          // 7
+        fileTaskManager.createSubtask(subtask21);   // 8
+
+        // вызываем задачи, заполняем историю менеджера, обновляем файл сохранения
+        fileTaskManager.getTaskById(1);
+        fileTaskManager.getTaskById(1);
+        fileTaskManager.getTaskById(2);
+        fileTaskManager.getTaskById(2);
+        fileTaskManager.getEpicById(3);
+        fileTaskManager.getEpicById(3);
+        fileTaskManager.getSubtaskById(4);
+        fileTaskManager.getSubtaskById(4);
+        fileTaskManager.getSubtaskById(5);
+        fileTaskManager.getSubtaskById(6);
+        fileTaskManager.getSubtaskById(6);
+        fileTaskManager.getEpicById(7);
+        fileTaskManager.getEpicById(7);
+        fileTaskManager.getSubtaskById(8);
+        fileTaskManager.getSubtaskById(8);
+
+        for (Task task : fileTaskManager.getHistory()) {         // проверка содержания менеджера истории
+            System.out.print(task.getId() + " ");
+            System.out.println(task);
+        }
+        System.out.println();
+
+        FileBackedTaskManager loadedTaskManager = FileBackedTaskManager.loadFromFile(file);        // создаем новый менеджер из файла
+
+        for (Task task : loadedTaskManager.getHistory()) {       // проверка содержания истории загруженного менеджера
+            System.out.print(task.getId() + " ");
+            System.out.println(task);
+        }
     }
 
     public static FileBackedTaskManager getDefaultFile(File file) {
@@ -53,11 +125,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
             writer.write("\n");
             writer.write(historyToString(getHistoryObject()));
         } catch (IOException e) {
-            try {
-                throw new ManagerSaveException("Ошибка сохранения файла!");
-            } catch (ManagerSaveException exp) {
-                System.out.println(exp.getMessage());
-            }
+            throw new ManagerSaveException("Ошибка сохранения файла!");
         }
     }
 
@@ -98,11 +166,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
                 newManager.getHistoryObject().add(loadedTaskMap.get(id));
             }
         } catch (IOException e) {
-            try {
-                throw new ManagerSaveException("Ошибка при чтении файла!");
-            } catch (ManagerSaveException exp) {
-                System.out.println(exp.getMessage());
-            }
+            throw new ManagerSaveException("Ошибка при чтении файла!");
         }
         return newManager;
     }
--- src/ru/yandex/praktikum/ivanov/kanban/managers/tasksManagers/ManagerSaveException.java
+++ src/ru/yandex/praktikum/ivanov/kanban/managers/tasksManagers/ManagerSaveException.java
@@ -1,6 +1,6 @@
 package ru.yandex.praktikum.ivanov.kanban.managers.tasksManagers;
 
-public class ManagerSaveException extends Exception {
+public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(final String message) {
         super(message);
     }
